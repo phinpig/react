59fc63cae0fbd903185d470f327de4bd9e79e267
lee <phinpig@gmail.com>
2022-10-26 18:30:23 +0900

    update

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppCounter from './AppCounter';
+import AppProducts from './AppProducts';
 import './index.css';
 // import AppProfile from './AppProfile';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <AppCounter />
+        <AppProducts />
     </React.StrictMode>
 );
 
